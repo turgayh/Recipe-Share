1f1dd2cc8f42f63ff3bbe03d85e9900b53c73d03
hakan turğay <turgayh@itu.edu.tr>
2020-02-02 16:13:53 +0300

    edit page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from restaurant import views
 
+
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path(r'^(?P<id>\d+)/edit/$', views.RecipeUpdate.as_view(), name='edit'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Recipe as RecipeModel
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,7 +10,14 @@ from django.contrib.auth.decorators import login_required
 class RecipeList(generic.ListView):
     queryset = RecipeModel.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+
+
 class RecipeDetail(generic.DetailView):
     model = RecipeModel
     template_name = 'recipe_detail.html'
 
+
+class RecipeUpdate(generic.UpdateView):
+    model = RecipeModel
+    fields = ['name']
+    template_name = 'edit.html'
